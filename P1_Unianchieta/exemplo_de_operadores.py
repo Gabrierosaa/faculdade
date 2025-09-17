d698882406766342f168c628513610a731887846
Gabriel Rosa <gabrielflororosa2007@gmail.com>
2025-09-16 22:10:50 -0300

    parte_2

---
+++ P1_Unianchieta/exemplo_de_operadores.py
@@ -0,0 +1,22 @@
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite outro numero: "))
+
+#Operadores Aritméticos
+print("Soma: ", numero1 + numero2)
+print("Subtração: ", numero1 - numero2)
+print("Multiplicação: ", numero1 * numero2)
+print("Divisão: ", numero1 / numero2)
+print("Divisão Inteira: ", numero1 // numero2)
+print("Resto da Divisão: ", numero1 % numero2)
+print("Exponenciação: ", numero1 ** numero2)
+#Comparação
+print("Número 1 é maior que Número 2? ", numero1 > numero2)
+print("Número 1 é menor que Número 2? ", numero1 < numero2)
+print("Número 1 é igual a Número 2? ", numero1 == numero2)
+print("Número 1 é diferente de Número 2? ", numero1 != numero2)
+print("Número 1 é maior ou igual a Número 2? ", numero1 >= numero2)
+print("Número 1 é menor ou igual a Número 2? ", numero1 <= numero2)
+#Operadores Lógicos
+print("Número 1 é maior que 5 e Número 2 é menor que 10? ", numero1 > 5 and numero2 < 10)
+print("Número 1 é maior que 5 ou Número 2 é menor que 10? ", numero1 > 5 or numero2 < 10)
+print("Número 1 não é maior que Número 2? ", not (numero1 > numero2))
