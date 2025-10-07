60b29787af54cd1413f106f9b7123cb68f9d6191
Gabriel Rosa <gabrielflororosa2007@gmail.com>
2025-10-07 19:30:45 -0300

    Add Aula_07_parte1.py with string examples

---
+++ Aula_07_parte1.py
@@ -0,0 +1,25 @@
+nome = "Joao"
+nome1 = 'Joao'
+
+texto_longo = """Este é um texto
+que possui varias
+linhas"""
+
+string_vazia = ""
+string_vazia2 = ''
+
+numero_string = str(123)
+
+#quebra de linha
+texto3 = "Primeira Linha \n Segunda linha"
+
+#tabulacao
+texto_tab = "Nome:\tJoão" #ele da o espaço do tab
+print(texto_tab)
+
+Frase = "She's Happy" #pra usar aspas dentro de uma string
+Frase2 = '"Frase com aspas"' #pra usar aspas dentro de uma string
+print(Frase)
+print(Frase2)
+
+caminho = "C:\\ " #para passar caminhos
