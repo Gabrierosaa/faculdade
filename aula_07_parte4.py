d6ae7b93966e44c8bf5d3de246e611c0bf04739b
Gabriel Rosa <gabrielflororosa2007@gmail.com>
2025-10-07 20:04:46 -0300

    Implement string indexing and slicing examples
    Added examples of string indexing and slicing in Python.

---
+++ aula_07_parte4.py
@@ -0,0 +1,24 @@
+palavra = ("Python")
+print(palavra[-6])
+print(palavra[-5])
+print(palavra[-4])
+print(palavra[-3])
+print(palavra[-2])
+print(palavra[-1])
+
+palavra1 = ("Python")
+print(palavra1[5])
+print(palavra1[4])
+print(palavra1[3])
+print(palavra1[2])
+print(palavra1[1])
+print(palavra1[0])
+
+palavra2 = "Programação"
+print(palavra2[:5]) # vai ate o index 5
+print(palavra2[5:])# vai ate o final
+print(palavra2[:])# a palavra toda
+#slicing
+print(palavra2[::2]) #step [são pulos]
+print(palavra2[::-1]) #step só q de tras pra frente
+print(palavra2[::1]) #step [são pulos]
