4dd5b145336f2cedec31f29fadfe71182ed25a17
Gabriel Rosa <gabrielflororosa2007@gmail.com>
2025-10-07 20:24:20 -0300

    Add basic string manipulations in aula_07_exercicio1.py

---
+++ aula_07_exercicio1.py
@@ -0,0 +1,12 @@
+boa = "boa"
+noite = "noite"
+boa_noite = boa + " " + noite
+print(boa_noite)
+
+teste = "Olá! " + " "
+print(teste * 10)
+
+palavra = "programação"
+print(palavra[2], "terceira letra")
+print(palavra[-1], "ultima letra")
+print(palavra[3:8]) #da posição 3 até a 7
