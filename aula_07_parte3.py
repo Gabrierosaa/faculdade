f61318825b646b20c936ece14b32e9172ffc1ae8
Gabriel Rosa <gabrielflororosa2007@gmail.com>
2025-10-07 19:45:04 -0300

    Create aula_07_parte3.py with string examples
    Add basic Python script demonstrating string formatting and repetition.

---
+++ aula_07_parte3.py
@@ -0,0 +1,25 @@
+idade = 22
+mensagem = f"eu tenho {idade} anos"
+print(mensagem)
+
+idade2 = 22
+mensagem2 = "Eu tenho {} anos.".format(idade2)
+print(mensagem2)
+
+p1 = "Eu gosto"
+p2 = "Muito de"
+p3 = "Programar em python"
+frase = p1 + "" + p2 + "" + p3
+print(frase)
+
+linha = "rs" * 20
+print(linha)
+
+separador = "=" * 22
+print(separador)
+print("Bem vindo ao sistema X")
+print(separador)
+
+qnt = 5
+texto = "Python!" * qnt
+print(texto)
