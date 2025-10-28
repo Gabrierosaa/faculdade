c8505f8f2dd1df2f095f456711d0ddd88460ee7d
Gabriel Rosa <gabrielflororosa2007@gmail.com>
2025-10-28 19:45:32 -0300

    Add mathematical functions in aula29-10parte2.py

---
+++ aula29-10parte2.py
@@ -0,0 +1,10 @@
+import math as m
+
+def matematica(numero):
+    print(m.isqrt(numero))
+    print(m.factorial(numero))
+    print(m.cos(numero))
+
+numero = int(input("Entre com vosso numero: "))
+matematica(numero)
+print(matematica)
